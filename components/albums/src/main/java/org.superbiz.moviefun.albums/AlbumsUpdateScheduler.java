bfcc1c34e38781c509a05a138f11b235c02013e3
diana pardy <dianapardy91@gmail.com>
2019-05-15 16:31:30 -0700

    initial

--- components/albums/src/main/java/org.superbiz.moviefun.albums/AlbumsUpdateScheduler.java
+++ components/albums/src/main/java/org.superbiz.moviefun.albums/AlbumsUpdateScheduler.java
@@ -3,6 +3,7 @@ package org.superbiz.moviefun.albums;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
